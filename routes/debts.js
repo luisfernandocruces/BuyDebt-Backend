0537cba2f9fd1ed443be61d7e77dd91ab697936e
luisfcv97 <luisfcv97@gmail.com>
2020-05-21 15:33:35 -0500

    fixing problems for deploy

--- routes/debts.js
+++ routes/debts.js
@@ -3,14 +3,9 @@ var router = express.Router();
 const debtController = require("../controllers/debt");
 
 router.post("/debt", debtController.registerDebt);
-<<<<<<< HEAD
-router.get("/debt", debtController.getDebts);
-=======
 
 router.get("/:id", debtController.getDebtsByUser);
 
-router.get('/debt', debtController.getDebts);
-
->>>>>>> b594887bce07a07051a70e8e41928692fa61ad31
+router.get("/debt", debtController.getDebts);
 
 module.exports = router;
